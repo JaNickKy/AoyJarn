ae7c6da2732bccc8385543db9db410fb8aa4b6c9
JaNickKy <NickZa4346@hotmail.com>
2023-10-17 03:05:38 +0700

    init project

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+static bool IsPrime(int x)
+{
+    if (x < 2)
+    {
+        return false;
+    }
+    else
+    {
+        for (int i = 2; i < x; i++)
+        {
+            if (x % i == 0)
+                return false;
+        }
+    }
+    return true;
+}
+int n;
+string input = string.Empty;
+while (input.Equals("w")) ;
+{
+    Console.Write("Enter Number:");
+    input = Console.ReadLine();
+    Int32.TryParse(input, out n);
+    
+    Console.Write("");
+    for (int i = 2; i <= n; i++)
+    
+    if (IsPrime(i))
+    
+    Console.Write(i+ ",");
+}
